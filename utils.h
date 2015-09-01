ef7e5fae9d94eab3fd8613dc045d6ac8a5b3f698
Bassel-Bakr <basselbakr@gmail.com>
2015-09-01 10:43:54 +0200

    format

--- utils.h
+++ utils.h
@@ -1,10 +1,14 @@
 #ifndef __UTILS_H__
-#define __UTILS_H__
-#include "constants.h"
+#  define __UTILS_H__
+#  include "constants.h"
 
+// Read
 int get_int(cstring file);
-
 shared_string get_line(cstring file);
+
+// Write
+bool write_int(cstring file);
+bool write_line(cstring file);
 
 
 #endif
